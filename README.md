116bbd913f976269ee8444f053b7fe7658ee1d9b
Jakob Buis <jpabuis@gmail.com>
2014-04-16 20:13:07 +0200

    use memcache to cache the results

--- README.md
+++ README.md
@@ -3,5 +3,11 @@ play2
 
 Game chooser tool for the BolkLAN, second version
 
+## Requirements
+* nginx
+* PHP5 with the memcache and mongodb modules
+* MongoDB server
+* Memcache server
+
 ## License
 Copyright Jakob Buis 2014. All rights reserved.
--- app/controllers/InterfaceController.php
+++ app/controllers/InterfaceController.php
@@ -2,6 +2,12 @@
 
 class InterfaceController extends BaseController
 {
+    public function __construct()
+    {
+        $this->memcache = new Memcached();
+        $this->memcache->addServer('localhost', 11211);    
+    }
+
 	public function index()
 	{
 		return View::make('index');
@@ -9,26 +15,38 @@ class InterfaceController extends BaseController
 
     public function timeslots()
     {
-        $timeslots = Timeslot::current();
+        // Load the timeslots using write-through caching
+        $timeslots = $this->memcache->get('timeslots');
 
-        //FIXME cache using memcache
-        return Response::json($timeslots);
+        if (! $timeslots) {
+            $timeslots = Response::json(Timeslot::current());
+            $this->memcache->set('timeslots', $timeslots);
+        }
+
+        return $timeslots;
     }
 
     public function choose()
     {
+        // Very basic validation
         if (!Input::has('name')) {
             App::abort(400, "Must enter a name");
         }
 
+        // Find the current timeslot
         $timeslot = Timeslot::find(Input::get('timeslot'));
         if (!$timeslot) {
             App::abort(404, 'Timeslot does not exist');
         }
 
-        // Set the timeslot
+        // Add the name to the timeslot
         $timeslot->set(Input::get('name'));
 
-        return $this->timeslots();
+        // Update memcache and return the updated set
+        $new_timeslots = Response::json(Timeslot::current());
+        $this->memcache->set('timeslots', $new_timeslots);
+
+        // Return updated set of timeslots
+        return $new_timeslots;
     }
 }
